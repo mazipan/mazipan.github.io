23e3979e6676fa7a6e18192f2f841f1d5834b4eb
Irfan Maulana <irfan.maulana@gdn-commerce.com>
2016-09-23 13:12:49 +0700

    add node starter

---
+++ Gruntfile.js
@@ -0,0 +1,39 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({        
+        pkg: grunt.file.readJSON('package.json'),
+            // SASS for compile scss into css
+            sass: {
+                dist: {
+                    options:{
+                        sourcemap: 'auto',
+                        style: 'compact',
+                        update: true
+                    },
+                    files: [{
+                        expand: true,
+                        cwd:  '_sass',
+                        src:  ['**/*.scss'],
+                        dest: 'css',
+                        ext:  '.css'
+                    }]                            
+
+                }
+            }
+
+    });
+
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-babel');
+
+    grunt.registerTask('default', ['sass']);
+};
