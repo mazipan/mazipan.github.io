2702c895eaa7bad5a0b7fabba4101854bfd42bda
Irfan Maulana <irfan.maulana@gdn-commerce.com>
2017-03-09 11:48:28 +0700

    remove service worker first

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+'use strict'
+
+navigator.serviceWorker.getRegistrations().then(function(registrations) {
+ for(let registration of registrations) {
+  registration.unregister()
+} });
+
+/*
 navigator
   .serviceWorker
   .register('service-worker.js', {
     scope: '/'
 });
-
+*/
