297a0252d6a4f9d4fd9d0d04f18563d36658d5d0
Irfan Maulana <irfan.maulana@gdn-commerce.com>
2017-03-09 11:42:01 +0700

    add g-adsense.js

---
+++ library/g-adsense.js
@@ -0,0 +1,17 @@
+function loaderScript(src){
+    var scripts = document.createElement('script');
+	scripts.setAttribute("type","text/javascript");
+	scripts.setAttribute("src", src);
+	scripts.setAttribute("async", true);
+
+	document.getElementsByTagName("head")[0].appendChild(scripts);
+    return scripts;
+}
+
+
+loaderScript('//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js');
+
+(adsbygoogle = window.adsbygoogle || []).push({
+google_ad_client: "ca-pub-5442972248172818",
+enable_page_level_ads: true
+});
